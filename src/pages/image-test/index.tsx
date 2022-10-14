40c2da2cd44ba12153d51f6c104c8730f42669b1
dlwnsgml0328 <junhee@eazel.net>
2022-10-14 14:21:57 +0900

    [test] test 2

--- src/pages/image-test/index.tsx
+++ src/pages/image-test/index.tsx
@@ -1,8 +1,22 @@
+import Head from 'next/head';
 import Image from 'next/image';
 
 const Test = () => {
   return (
     <div>
+      <Head>
+        <title>Test</title>
+        <meta name="description" content="test image" />
+
+        <meta name="twitter:card" content="summary_large_image" data-rh="true" />
+        <meta name="twitter:title" content="original" data-rh="true" />
+        <meta name="twitter:description" content="Image twitter" data-rh="true" />
+        <meta
+          name="twitter:image"
+          content="https://upload.wikimedia.org/wikipedia/commons/b/b7/Mandelbrot_set_5000px.png"
+          data-rh="true"
+        />
+      </Head>
       <h1>Image test</h1>
       <div>
         <Image
